1faed565d5588806a0ff2dd2bfce397f09e4c6d1
DESKTOP-BS3VQM6\Administrator <978180814@qq.com>
2026-08-25 14:38:46 +0800

    修复分页渲染全部页码：改用 Laravel 窗口分页（elements + 省略号）
    - 原因：custom.blade.php 用 getUrlRange(1, lastPage) 全量渲染，100万数据 66667 页全部输出
    - 修复：改用 links() 传入的 \ 窗口（onEachSide(2)），首尾窗口 + 省略号
    - 验证：第1页 1..10/66666/66667；第66000页 1,2/65997-66003/66666,66667

--- business-system/resources/views/pagination/custom.blade.php
+++ business-system/resources/views/pagination/custom.blade.php
@@ -1,4 +1,5 @@
 @if ($paginator->hasPages())
+    @php $paginator->onEachSide(2); @endphp
     {{-- 上一页 --}}
     @if ($paginator->onFirstPage())
         <span class="disabled">« 上一页</span>
@@ -6,9 +7,15 @@
         <a href="{{ $paginator->previousPageUrl() }}">« 上一页</a>
     @endif
 
-    {{-- 页码 --}}
-    @foreach ($paginator->getUrlRange(1, $paginator->lastPage()) as $page => $url)
-        <a href="{{ $url }}" @class(['active' => $page == $paginator->currentPage()])>{{ $page }}</a>
+    {{-- 页码（窗口模式：首尾 + 当前页前后 2 页，中间用省略号） --}}
+    @foreach ($elements as $element)
+        @if (is_string($element))
+            <span class="disabled">…</span>
+        @else
+            @foreach ($element as $page => $url)
+                <a href="{{ $url }}" @class(['active' => $page == $paginator->currentPage()])>{{ $page }}</a>
+            @endforeach
+        @endif
     @endforeach
 
     {{-- 下一页 --}}
